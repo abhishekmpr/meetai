eda34b7905fa233bca8bd99b7dcdced04b799171
Abhishek <aaabhishekmishra123@gmail.com>
2025-09-18 23:35:30 +0530

    02:Database

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,5 +2,7 @@ import Image from "next/image";
 import { Button } from "@/components/ui/button";
 
 export default function Home() {
- return(<Button variant="destructive">clickme</Button>)
+ return(<Button variant="destructive">clickme</Button>
+  
+ )
 }
